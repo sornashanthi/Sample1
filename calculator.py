68d0ea58d9245989dff7fb644f41eededc10ecbf
sornashanthi <sornashanthi.d@rajalakshmi.edu.in>
2024-03-01 20:29:06 +0530

    big fixed

--- calculator.py
+++ calculator.py
@@ -8,29 +8,32 @@ def subtract(x, y):
 
 # Function to divide two numbers
 def divide(x, y):
-        return x / y
- print("Select operation.")
-    print("1.Add")
-    print("2.Subtract")
-    print("4.Divide")
+    if(y==0):
+        return "Dividing by zero"
+    return x / y
 
-    while True:
-        # Take input from the user
-        choice = input("Enter choice(1/2/3): ")
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("4.Divide")
 
-        # Check if choice is one of the four options
-        if choice in ('1', '2', '3'):
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+while True:
+# Take input from the user
+choice = input("Enter choice(1/2/3): ")
 
-            if choice == '1':
-                print(num1, "+", num2, "=", add(num1, num2))
+# Check if choice is one of the four options
+if choice in ('1', '2', '3'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-            elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1, num2))
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
 
-            elif choice == '3':
-                print(num1, "*", num2, "=", multiply(num1, num2))
-        else:
-            print("Invalid Input")
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+else:
+    print("Invalid Input")
 
