d0196eb0e9e05d60bb0fc17e31e91d367d8faecd
sornashanthi <sornashanthi.d@rajalakshmi.edu.in>
2024-03-01 21:02:56 +0530

    added multiply

--- calculator.py
+++ calculator.py
@@ -5,21 +5,23 @@ def add(x, y):
 # Function to subtract two numbers
 def subtract(x, y):
     return x - y
-
+def multiply(x,y):
+    return x*y
 # Function to divide two numbers
 def divide(x, y):
         return x / y
  print("Select operation.")
     print("1.Add")
     print("2.Subtract")
+    print("3.Multiply")
     print("4.Divide")
 
     while True:
         # Take input from the user
-        choice = input("Enter choice(1/2/3): ")
+        choice = input("Enter choice(1/2/3/4): ")
 
         # Check if choice is one of the four options
-        if choice in ('1', '2', '3'):
+        if choice in ('1', '2', '3','4'):
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
 
@@ -31,6 +33,9 @@ def divide(x, y):
 
             elif choice == '3':
                 print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+
         else:
             print("Invalid Input")
 
